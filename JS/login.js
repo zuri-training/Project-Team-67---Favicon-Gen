e08a325ee4265764531a6247bdbd596e4ceb3280
KrownWealth <abdulramonadeola36@gmail.com>
2022-08-12 14:53:17 -0700

    js for confirm password done

--- JS/login.js
+++ JS/login.js
@@ -1,2 +1,20 @@
-// Login form validation using JS, username minimum of 8 character length with no space validation
-// Password field minmum of 8 character length, at least one alphabet and one digits
+
+//Vaildation code for inputs
+
+var username = document.forms['form'][username];
+var password = document.forms['form'][user-pass];
+
+var name_error = document.getElementById(name_error);
+var pass_error = document.getElementById(pass_error);
+var submit_error = document.getElementById(submit_error);
+
+function validated(){
+    if(username.value.length < 9){
+        username.style.border = "1px solid red";
+        name_error.style.display = "block";
+        username.focus();
+        return false;
+
+    }
+
+}
