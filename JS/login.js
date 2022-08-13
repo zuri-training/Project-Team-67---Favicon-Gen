281138935b110689ba660c4e838d101d4c51a905
KrownWealth <abdulramonadeola36@gmail.com>
2022-08-13 08:39:11 -0700

    Re-arrange Nathaniel folders into the laid down guidelines

--- JS/login.js
+++
@@ -1,2 +0,0 @@
-// Login form validation using JS, username minimum of 8 character length with no space validation
-// Password field minmum of 8 character length, at least one alphabet and one digits
